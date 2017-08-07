f879edcea27246cdc9056bdb0a4511eebf09b483
ShenbaM <manthiram@gmail.com>
2017-08-07 22:16:01 +1000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile("Article one will be served");
+  ("Article one will be served");
 });
 
 app.get('/article-two', function (req, res) {
